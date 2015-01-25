21de0d3606086c4a213e977b5e0d26e717e7ff55
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-25 11:30:05 +0100

    Added states to our tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125093245) do
+ActiveRecord::Schema.define(version: 20150125100429) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150125093245) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: true do |t|
