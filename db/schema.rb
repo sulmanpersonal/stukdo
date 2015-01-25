15892313e8ecc316aae149b446116534057329a6
Sulman Baig <sulmanpersonal@gmail.com>
2015-01-25 10:57:56 +0100

    Add a user id to tasks, configured user task associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125084105) do
+ActiveRecord::Schema.define(version: 20150125093245) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150125084105) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
